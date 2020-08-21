59c6c1641cd9e6ca59079c13e65e913442012105
Leigh Johnson <hi@leighjohnson.me>
2020-08-21 13:23:29 -0700

    finish first pass on scraper module

--- annotator/scraper.py
+++ annotator/scraper.py
@@ -1,9 +1,10 @@
 
 import logging
+import json
 from typing import List
 from bs4 import BeautifulSoup
-#from pytube import YouTube
-#import cv2
+from pytube import YouTube
+import cv2
 import os
 import urllib
 from pyppeteer import launch
@@ -11,7 +12,10 @@ from pyppeteer import launch
 import aiohttp
 import asyncio
 
-async def get_urls(search: str, limit: int = 10 ) -> List[str]:
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+async def get_search_result_urls(search: str, limit: int = 10, offset: int=0) -> List[str]:
     browser = await launch({'headless': True})
     query = urllib.parse.quote(search)
     url = "https://www.youtube.com/results?search_query=" + query
@@ -19,14 +23,79 @@ async def get_urls(search: str, limit: int = 10 ) -> List[str]:
     results = await page.goto(url)
     elements = await page.querySelectorAll('#video-title')
     results = []
-    for i, el in enumerate( elements ):
-        if i < limit:
-            result = await page.evaluate('(element) => element.href', el)
+    for el in elements[offset:]:
+        result = await page.evaluate('(element) => element.href', el)
+        
+        if result is not None:
             results.append(result)
-        else:
+        if len(results) == limit:
             break
+
     await browser.close()
     return results
+
+def download_video(url:str, path:str=f'{os.getcwd()}/tmp/') -> str:
+    yt = YouTube(url)
+
+    out = yt.streams.filter(adaptive=True, res="480p").first()
+
+    video_id = yt.video_id
+
+    out_path = path + video_id
+    out_file = out.download(out_path, filename='video')
+
+    metadata_file = f'{out_path}/metadata.json'
+    metadata = {
+        'video_filename': 'video.mp4',
+        'path': out_path,
+        'video_id': video_id,
+        'title': yt.title,
+        'url': url,
+        'stream': {
+            'mime_type': out.mime_type,
+            'fps': out.fps,
+            'video_codec': out.video_codec,
+            'resolution': out.resolution
+        }
+    }
+    with open(metadata_file, 'w+') as f:
+        json.dump(metadata, f)
+
+    logging.info(f"Finished download {out_file} from {url}")
+    return metadata
+
+def video_to_image_frames(metadata:dict, folder:str=f'{os.getcwd()}/tmp/images'):    
+
+    metadata_file = os.path.join(metadata['path'], 'metadata.json')
+
+    if metadata.get('frame_count') is not None:
+        logger.warning(f'Skipping {metadata["path"]} - frames already extracted')
+        return False, metadata
+        
+    video_path = os.path.join(metadata['path'], metadata['video_filename'])
+    vidcap = cv2.VideoCapture(video_path)
+    success,image = vidcap.read()
+    count = 0
+
+
+    out_path = metadata['path'] + '/frames'
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
+
+    while success:
+        cv2.imwrite(f'{out_path}/{count}.jpg', image)      
+        success,image = vidcap.read()
+        logger.info(f'Read a new frame: {count} success: {success}')
+        count += 1
+
+    metadata['frame_count'] = count
+    metadata['frame_path'] = out_path
+    with open(metadata_file, 'w+') as f:
+        json.dump(metadata, f, indent=2)
+    
+    return True, metadata
+
+
 
 # async def fetch(session, url):
 #     async with session.get(url) as response:
@@ -36,76 +105,3 @@ async def get_urls(search: str, limit: int = 10 ) -> List[str]:
 #     async with aiohttp.ClientSession() as session:
 #         html = await fetch(session, 'http://python.org')
 #         print(html)
-
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-
-
-
-
-# def download_video(url, path=None, max_duration=10):
-#       try:
-#         yt = YouTube(url)
-#         duration = int(yt.player_config_args['player_response']['streamingData']['formats'][0]['approxDurationMs'])
-#         if duration < max_duration*60*1000:
-#             yt = yt.streams.filter(file_extension='mp4').first()
-#             out_file = yt.download(path)
-#             file_name = out_file.split("\\")[-1]
-#             print(f"Downloaded {file_name} correctly!")
-#         else:
-#             print(f"Video {url} too long")
-#     except Exception as exc:
-#         print(f"Download of {url} did not work because of {exc}...")
-
-
-# def extract_images_from_video(video, folder=None, delay=30, name="file", max_images=20, silent=False):    
-#     vidcap = cv2.VideoCapture(video)
-#     count = 0
-#     num_images = 0
-#     if not folder:
-#         folder = os.getcwd()
-#     label = max_label(name, folder)
-#     success = True
-#     fps = int(vidcap.get(cv2.CAP_PROP_FPS))
-    
-#     while success and num_images < max_images:
-#         success, image = vidcap.read()
-#         num_images += 1
-#         label += 1
-#         file_name = name + "_" + str(label) + ".jpg"
-#         path = os.path.join(folder, file_name)
-#         cv2.imwrite(path, image)
-#         if cv2.imread(path) is None:
-#             os.remove(path)
-#         else:
-#             if not silent:
-#                 print(f'Image successfully written at {path}')
-#         count += delay*fps
-#         vidcap.set(1, count)
-
-
-# def extract_images_from_video(video, folder=None, delay=30, name="file", max_images=20, silent=False):    
-#     vidcap = cv2.VideoCapture(video)
-#     count = 0
-#     num_images = 0
-#     if not folder:
-#         folder = os.getcwd()
-#     label = max_label(name, folder)
-#     success = True
-#     fps = int(vidcap.get(cv2.CAP_PROP_FPS))
-    
-#     while success and num_images < max_images:
-#         success, image = vidcap.read()
-#         num_images += 1
-#         label += 1
-#         file_name = name + "_" + str(label) + ".jpg"
-#         path = os.path.join(folder, file_name)
-#         cv2.imwrite(path, image)
-#         if cv2.imread(path) is None:
-#             os.remove(path)
-#         else:
-#             if not silent:
-#                 print(f'Image successfully written at {path}')
-#         count += delay*fps
-#         vidcap.set(1, count)
--- test/test_scraper.py
+++ test/test_scraper.py
@@ -1,7 +1,30 @@
+import os
 import pytest
-from annotator.scraper import get_urls
+from annotator.scraper import get_search_result_urls, download_video, video_to_image_frames
 
+# ['https://www.youtube.com/watch?v=aWg1nG2AbA8', 'https://www.youtube.com/watch?v=YS6gH66_7yE', 'https://www.youtube.com/watch?v=X-dQ2r0YI4Y']
 @pytest.mark.asyncio
-async def test_get_urls():
-    urls = await get_urls("3d printer spaghetti", limit=3)
+async def test_get_search_result_urls():
+    urls = await get_search_result_urls("3d printer spaghetti", limit=3)
     assert len(urls) == 3
+
+def test_download_video():
+    url = 'https://www.youtube.com/watch?v=aWg1nG2AbA8'
+    metadata = download_video(url)
+    assert metadata == { 'video_filename': 'video.mp4', 'path': f'{os.getcwd()}/tmp/aWg1nG2AbA8', 'video_id': 'aWg1nG2AbA8', 'title': '5 3D Printing Mistakes you WILL make - and how to avoid them! 3D Printing 101', 'url': 'https://www.youtube.com/watch?v=aWg1nG2AbA8', 'stream': {'mime_type': 'video/mp4', 'fps': 30, 'video_codec': 'avc1.4d401f', 'resolution': '480p'}}
+
+@pytest.mark.slow
+def test_write_video_image_frames():
+    metadata_in = { 'video_filename': 'video.mp4', 'path': f'{os.getcwd()}/tmp/aWg1nG2AbA8', 'video_id': 'aWg1nG2AbA8', 'title': '5 3D Printing Mistakes you WILL make - and how to avoid them! 3D Printing 101', 'url': 'https://www.youtube.com/watch?v=aWg1nG2AbA8', 'stream': {'mime_type': 'video/mp4', 'fps': 30, 'video_codec': 'avc1.4d401f', 'resolution': '480p'}}
+    success, metadata_out = video_to_image_frames(metadata_in)
+    assert success == True
+    assert metadata_out == {'video_filename': 'video.mp4', 'path': '/home/leigh/projects/youtube-data-annotator/tmp/aWg1nG2AbA8', 'video_id': 'aWg1nG2AbA8', 'title': '5 3D Printing Mistakes you WILL make - and how to avoid them! 3D Printing 101', 'url': 'https://www.youtube.com/watch?v=aWg1nG2AbA8', 'stream': {'mime_type': 'video/mp4', 'fps': 30, 'video_codec': 'avc1.4d401f', 'resolution': '480p'}, 'frame_count': 15244, 'frame_path': '/home/leigh/projects/youtube-data-annotator/tmp/aWg1nG2AbA8/frames'}
+
+# skip writing frames if metadata already includes frame_count
+def test_skip_write_video_image_frames():
+    metadata_in = {'video_filename': 'video.mp4', 'path': '/home/leigh/projects/youtube-data-annotator/tmp/aWg1nG2AbA8', 'video_id': 'aWg1nG2AbA8', 'title': '5 3D Printing Mistakes you WILL make - and how to avoid them! 3D Printing 101', 'url': 'https://www.youtube.com/watch?v=aWg1nG2AbA8', 'stream': {'mime_type': 'video/mp4', 'fps': 30, 'video_codec': 'avc1.4d401f', 'resolution': '480p'}, 'frame_count': 15244, 'frame_path': '/home/leigh/projects/youtube-data-annotator/tmp/aWg1nG2AbA8/frames'}
+    success, metadata_out = video_to_image_frames(metadata_in)
+    assert success == False
+    assert metadata_out == {'video_filename': 'video.mp4', 'path': '/home/leigh/projects/youtube-data-annotator/tmp/aWg1nG2AbA8', 'video_id': 'aWg1nG2AbA8', 'title': '5 3D Printing Mistakes you WILL make - and how to avoid them! 3D Printing 101', 'url': 'https://www.youtube.com/watch?v=aWg1nG2AbA8', 'stream': {'mime_type': 'video/mp4', 'fps': 30, 'video_codec': 'avc1.4d401f', 'resolution': '480p'}, 'frame_count': 15244, 'frame_path': '/home/leigh/projects/youtube-data-annotator/tmp/aWg1nG2AbA8/frames'}
+
+
